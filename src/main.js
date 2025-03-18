a250efc1c341dbea0e1e1380e7918f089a93b2a8
Cristhian Daza <cristian.d.b@hotmail.com>
2025-03-17 19:34:42 -0500

    fix(main): replace vue-highlight-code CSS import with github-markdown-css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import TvCard from './demo/Demo.vue'
-import "vue-highlight-code/dist/style.css";
+import 'github-markdown-css';
 import '../src/assets/scss/style.scss'
 import '../src/assets/scss/_global.scss'
 createApp(TvCard).mount('#tv-card')
